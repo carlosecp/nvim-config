60a41f6ba294aea8ae1a06cd61e7865d19c604d4
Carlos Castillo <47466248+itscarlosecp@users.noreply.github.com>
2021-08-13 09:15:46 -0300

    Removed LSPInstall again

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,6 +1,3 @@
-local lspinstall = require "lspinstall"
-local lspconfig = require "lspconfig"
-
 vim.fn.sign_define(
   "LspDiagnosticsSignError",
   {texthl = "LspDiagnosticsSignError", text = "", numhl = "LspDiagnosticsSignError"}
@@ -59,37 +56,3 @@ vim.lsp.protocol.CompletionItemKind = {
   "   (Operator)",
   "   (TypeParameter)"
 }
-
-local function common_on_attach()
-	vim.cmd[[nnoremap <silent> K  <cmd>lua vim.lsp.buf.hover()          <CR>]]
-	vim.cmd[[nnoremap <silent> gd <cmd>lua vim.lsp.buf.definition()     <CR>]]
-	vim.cmd[[nnoremap <silent> gD <cmd>lua vim.lsp.buf.declaration()    <CR>]]
-	vim.cmd[[nnoremap <silent> gs <cmd>lua vim.lsp.buf.signature_help() <CR>]]
-end
-
-local configs = {
-	json = require "lsp.configs.json",
-	lua = require "lsp.configs.lua",
-}
-
-local function setup_servers()
-  lspinstall.setup()
-  local servers = lspinstall.installed_servers()
-  for _, server in pairs(servers) do
-    local client = lspconfig[server]
-    local config = configs[server] or client
-    client.setup {
-      filetypes = config.filetypes or client.filetypes,
-      on_attach = config.on_attach or common_on_attach,
-      settings  = config.settings or {}
-    }
-  end
-end
-
-setup_servers()
-
--- Automatically reload after `:LspInstall <server>` so we don"t have to restart neovim
-require "lspinstall".post_install_hook = function ()
-  setup_servers() -- reload installed servers
-  vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
-end
