605ad029c5a42604038cee8da862a594225f4678
Carlos Castillo <itscarlosecp@gmail.com>
2021-08-18 12:21:12 -0300

    Saving

--- lua/plugins/nvimtree/init.lua
+++ lua/plugins/nvimtree/init.lua
@@ -6,17 +6,24 @@ vim.g.nvim_tree_tab_open       = 1
 vim.g.nvim_tree_special_files  = {}
 vim.g.nvim_tree_indent_markers = 0
 
+vim.g.nvim_tree_show_icons = {
+  git = 1,
+	folders = 1,
+	files = 1,
+	folder_arrows = 0
+}
+
 vim.g.nvim_tree_icons = {
 	default = "",
   symlink = "",
   git = {
-    unstaged  = "",
-    staged    = "",
-    unmerged  = "",
-    renamed   = "",
-    untracked = "",
-    deleted   = "",
-    ignored   = ""
+    unstaged  = "•",
+    staged    = "•",
+    unmerged  = "•",
+    renamed   = "•",
+    untracked = "•",
+    deleted   = "•",
+    ignored   = "•"
   },
   folder = {
     default      = "",
