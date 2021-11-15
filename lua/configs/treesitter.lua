e7de61be3968b25eb8e0ebcdcdff1d57bdaeae81
Carlos Castillo <47466248+itscarlosecp@users.noreply.github.com>
2021-11-15 08:52:20 -0300

    Returning to json schemas working commit

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -2,7 +2,7 @@ require "nvim-treesitter.configs".setup {
 	ensure_installed = "all",
 	highlight = {
 		enable = true,
-		-- disable = { "html" },
+		disable = { "html" },
 		additional_vim_regex_highlighting = true
 	},
 	indent = {
--- lua/themes/solarized_nvim.lua
+++ lua/themes/solarized_nvim.lua
@@ -6,6 +6,6 @@
 -- }
 
 vim.g.solarized_italics = 0
+vim.g.solarized_termtrans = 1
 vim.cmd("colo solarized")
-vim.cmd("hi Normal guibg=NONE")
 vim.cmd("hi TSVariable guibg=NONE")
