220395d35d140c97812d507e1f3fedd1351c7504
Carlos Castillo <47466248+cecp01@users.noreply.github.com>
2022-01-08 18:52:43 -0300

    fix: updated onedark setup

--- lua/themes/onedark.lua
+++ lua/themes/onedark.lua
@@ -5,16 +5,19 @@
 	-- end
 -- }
 
-vim.g.onedark_transparent_background = true
-vim.g.onedark_italic_comment = false
-
 local status_ok, onedark = pcall(require, "onedark")
 if not status_ok then
 	return
 end
 
-onedark.setup()
+onedark.load()
+onedark.setup {
+	style = "dark",
+	transparent = false,
+	code_style = {
+		comments = "italic"
+	}
+}
 
 vim.cmd("hi VertSplit guifg=#21252b")
 vim.cmd("hi ColorColumn guibg=#21252b")
-vim.cmd("hi NvimTreeNormal guibg=#21252b")
